74fbf661d99c873664c79991db1148e4d9aa576d
priyasuresh2311 <priya.sg555@gmail.com>
2021-01-11 16:27:16 +0530

    New Java Project added

---
+++ src/org/prac/Child.java
@@ -0,0 +1,22 @@
+package org.prac;
+
+public class Child {
+
+	public void div() {
+		// TODO Auto-generated method stub
+		
+	}
+	public  void add() {
+		System.out.println("Addition");
+
+		
+	}
+public static void main(String[] args) {
+	Child a = new Child();
+	a.add();
+	a.div();
+	
+}	
+}
+
+	
